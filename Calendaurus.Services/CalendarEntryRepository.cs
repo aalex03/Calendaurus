a4fa0cb5128f3d4d6ed171955c3cb1b48f3d1b77
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-04-16 16:50:51 +0300

    add calendar entry repository

---
+++ Calendaurus.Services/CalendarEntryRepository.cs
@@ -0,0 +1,71 @@
+using Calendaurus.Models;
+
+namespace Calendaurus.Services;
+
+public class CalendarEntryRepository<T> : IRepository<CalendarEntry>
+{
+    public static List<CalendarEntry> _calendarEntries = [
+        new CalendarEntry
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test",
+            Description = "Test",
+            Start = DateTime.Now,
+            End = DateTime.Now,
+            Created = DateTime.Now,
+            Updated = DateTime.Now,
+            Type = 1,
+            Location = "Test"
+        },
+        new CalendarEntry
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test2",
+            Description = "Test2",
+            Start = DateTime.Now,
+            End = DateTime.Now,
+            Created = DateTime.Now,
+            Updated = DateTime.Now,
+            Type = 2,
+            Location = "Test2"
+        }
+    ];
+    public Task<CalendarEntry> CreateAsync(CalendarEntry entity)
+    {
+        _calendarEntries.Add(entity);
+        return Task.FromResult(entity);
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        var entry = _calendarEntries.FirstOrDefault(x => x.Id == id);
+        if (entry == null)
+        {
+            return Task.FromResult(false);
+        }
+        _calendarEntries.Remove(entry);
+        return Task.FromResult(true);
+    }
+
+    public Task<IEnumerable<CalendarEntry>> GetAllAsync()
+    {
+        return Task.FromResult(_calendarEntries.AsEnumerable());
+    }
+
+    public Task<CalendarEntry?> GetAsync(Guid id)
+    {
+        return Task.FromResult(_calendarEntries.FirstOrDefault(x => x.Id == id));
+    }
+
+    public Task<CalendarEntry?> UpdateAsync(CalendarEntry entity)
+    {
+        var entry = _calendarEntries.Find(x => x.Id == entity.Id);
+        if(entry is not null)
+        {
+            _calendarEntries.Remove(entry);
+            _calendarEntries.Add(entity);
+            return Task.FromResult<CalendarEntry?>(entity);
+        }
+        return Task.FromResult(entry);
+    }
+}
--- Calendaurus.Services/IRepository.cs
+++ Calendaurus.Services/IRepository.cs
@@ -3,8 +3,8 @@ namespace Calendaurus.Services;
 public interface IRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync();
-    Task<T> GetAsync(Guid id);
+    Task<T?> GetAsync(Guid id);
     Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
+    Task<T?> UpdateAsync(T entity);
     Task<bool> DeleteAsync(Guid id);
 }
