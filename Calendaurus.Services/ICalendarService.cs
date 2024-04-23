9c08e4445f84ba54fccdd0ecf33d7f9fecc19971
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-04-23 18:10:56 +0300

    update repository interface

--- Calendaurus.Services/ICalendarService.cs
+++ Calendaurus.Services/ICalendarService.cs
@@ -2,8 +2,10 @@ namespace Calendaurus.Services;
 using Calendaurus.Models;
 public interface ICalendarService
 {
-    Task<CalendarEntry> CreateAsync(CalendarEntry entry);
-    Task<CalendarEntry> UpdateAsync(CalendarEntry entry);
+    Task<CalendarEntry?> GetAsync(Guid id);
+    Task<IEnumerable<CalendarEntry>> GetAllAsync();
+    Task<CalendarEntry?> CreateAsync(CalendarEntry entry);
+    Task<CalendarEntry?> UpdateAsync(CalendarEntry entry);
     Task<bool> DeleteAsync(Guid id);
 }
 
--- Calendaurus.Services/IRepository.cs
+++ Calendaurus.Services/IRepository.cs
@@ -1,9 +1,13 @@
+using System.Linq.Expressions;
+using Calendaurus.Models;
+
 namespace Calendaurus.Services;
 
 public interface IRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync();
     Task<T?> GetAsync(Guid id);
+    Task<IEnumerable<T>> GetAsync(Expression<Func<CalendarEntry,bool>> predicate);
     Task<T> CreateAsync(T entity);
     Task<T?> UpdateAsync(T entity);
     Task<bool> DeleteAsync(Guid id);
