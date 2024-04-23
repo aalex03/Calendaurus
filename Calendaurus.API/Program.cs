e768e92b3d2bad80c2f9df18cd79095d21466365
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-04-23 18:34:34 +0300

    add authentication support

--- Calendaurus.API/Program.cs
+++ Calendaurus.API/Program.cs
@@ -1,18 +1,59 @@
+using System.IdentityModel.Tokens.Jwt;
 using Calendaurus.Models;
 using Calendaurus.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Enabled OAuth security in Swagger
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+    {
+        new OpenApiSecurityScheme {
+            Reference = new OpenApiReference {
+                Type = ReferenceType.SecurityScheme,
+                Id = "oauth2"
+            },
+            Scheme = "oauth2",
+            Name = "oauth2",
+            In = ParameterLocation.Header
+        },
+        new List <string> ()
+    }
+    });
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri("https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri("https://login.microsoftonline.com/consumers/oauth2/v2.0/token"),
+            }
+        }
+    });
+});
 builder.Services.AddScoped<ICalendarService, CalendarService>();
 builder.Services.AddScoped<IRepository<CalendarEntry>, CalendarEntryEfRepository<CalendarEntry>>();
 builder.Services.AddDbContext<CalendaurusContext>(options => options.UseSqlServer("Server=localhost;Database=Calendaurus;User Id=sa;Password=Alex123.;Encrypt=False", b => b.MigrationsAssembly("Calendaurus.API")));
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.TokenValidationParameters.NameClaimType = "preferred_username";
+    },
+    options => builder.Configuration.Bind("AzureAd", options));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
