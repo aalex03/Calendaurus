d93086ed3827ee5f9bef756b778ca059a0febaa1
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-04-23 18:11:20 +0300

    update model and add migrations

---
+++ Calendaurus.API/Migrations/20240416160356_CalendarEntries.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calendaurus.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CalendarEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalendarEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Type = table.Column<short>(type: "smallint", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarEntries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarEntries");
+        }
+    }
+}
--- Calendaurus.Models/CalendarEntry.cs
+++ Calendaurus.Models/CalendarEntry.cs
@@ -1,15 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Calendaurus.Models
 {
     public class CalendarEntry
     {
+        [Key]
         public Guid Id { get; set; }
+        [StringLength(200)]
         public string Title { get; set; } = default!;
+        [StringLength(2000)]
         public string? Description { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public DateTime Created { get; set; }
-        public DateTime Updated { get; set; }
+        public DateTime? Updated { get; set; }
         public short Type { get; set; }
+        [StringLength(200)]
         public string? Location { get; set; }
     }
 }
