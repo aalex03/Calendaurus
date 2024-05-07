6956e1b04932a3e1bd2433e852bb8e61e8398118
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-05-07 19:06:09 +0300

    users table rename migration

---
+++ Calendaurus.API/Migrations/20240507160419_UsersTableRename.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calendaurus.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersTableRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalendarEntries_User_UserId",
+                table: "CalendarEntries");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalendarEntries_Users_UserId",
+                table: "CalendarEntries",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalendarEntries_Users_UserId",
+                table: "CalendarEntries");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalendarEntries_User_UserId",
+                table: "CalendarEntries",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
