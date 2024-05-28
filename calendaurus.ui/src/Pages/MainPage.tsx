69ff611cff54927d6d4e72aa039b66030fc531b6
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-05-28 15:21:27 +0300

    add main page

--- calendaurus.ui/src/Pages/MainPage.tsx
+++ calendaurus.ui/src/Pages/MainPage.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { Calendar } from "../Components/Calendar";
+import { Header } from "../Components/Header";
+import dayjs from "dayjs";
+
 export const MainPage = () => {
-    <div>Main Page</div>
+    const [weekDates, setWeekDates] = useState<string[]>([]);
+    const changeWeek = (direction:string) => {
+        let newWeekDates: string[] = [];
+        if (direction === "next") {
+            newWeekDates = weekDates.map((date) => dayjs(date, "Do MMMM").add(1, "week").format("Do MMMM"));
+        } else if (direction === "previous"){
+            newWeekDates = weekDates.map((date) => dayjs(date, "Do MMMM").subtract(1, "week").format("Do MMMM"));
+        } else if(direction === "today") {
+            const startOfWeek = dayjs().startOf("week").add(1, "day");
+            newWeekDates = Array.from({ length: 7 }, (_, i) => startOfWeek.add(i, "day").format("Do MMMM"));
+        }
+        setWeekDates(newWeekDates);
+    }
+
+    useEffect(() => {
+        const startOfWeek = dayjs().startOf("week").add(1, "day");
+        const dateOfWeek = Array.from({ length: 7 }, (_, i) => startOfWeek.add(i, "day").format("Do MMMM"));
+        setWeekDates(dateOfWeek);
+    }, []);
+    return (
+        <div>
+            <Header changeWeekDates = {changeWeek}/>
+            <Calendar weekDays={weekDates} />
+        </div>
+    );
 }
