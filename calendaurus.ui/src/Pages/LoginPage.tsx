2412225deeec570a84dd5c8651e656294d95d7e1
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-03 10:43:37 +0300

    use navigation in login page

--- calendaurus.ui/src/Pages/LoginPage.tsx
+++ calendaurus.ui/src/Pages/LoginPage.tsx
@@ -1,13 +1,9 @@
 import { Button } from "@mui/material";
 import { useState } from "react";
-type Props = {
-  name: string;
-  lastName: string;
-}
-export const LoginPage = (props: Props) => {
-  const [counter, setCounter] = useState(0);
-  return <div style={{ display: "flex", flexDirection: "column-reverse", padding: "1rem" }}>
-    <h1>Login, {props.name}</h1>
-    <Button id="LoginButton" variant="contained" color="primary" style={{backgroundColor:(counter%2==0 ? "red" : "blue"), height: counter*10+"px"}}onClick={() => { setCounter(counter + 1); }}>Login tries: {counter}</Button>
+import { useNavigate } from "react-router-dom";
+export const LoginPage = () => {
+  const navigation = useNavigate();
+  return <div style={{ display: "flex", flexDirection: "column", padding: "1rem" }}>
+    <Button onClick={() => navigation("/home")}>Login</Button>
   </div>;
 }
