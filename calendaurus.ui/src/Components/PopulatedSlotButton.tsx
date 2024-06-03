22b085b4fbdeb8bff506fea0458112c8b6af14b3
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-03 11:18:24 +0300

    add delete dialog

--- calendaurus.ui/src/Components/PopulatedSlotButton.tsx
+++ calendaurus.ui/src/Components/PopulatedSlotButton.tsx
@@ -1,28 +1,28 @@
 import { Edit, MoreVert, Remove } from "@mui/icons-material"
-import { Box, Card, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Typography } from "@mui/material"
+import { Box, Card, Dialog, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Typography } from "@mui/material"
 import { ICalendarEntry } from "../types"
 import React from "react";
 import { EventModal } from "./EventModal";
 export type PopulatedSlotButtonProps = {
-    details: ICalendarEntry;
+    calendarEntry: ICalendarEntry;
 }
 
 export const PopulatedSlotButton = (props: PopulatedSlotButtonProps) => {
-    const { details } = props;
+    const { calendarEntry } = props;
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [openModal, setOpenModal] = React.useState(false);
+    const [openEditModal, setOpenEditModal] = React.useState(false);
+    const [openDeleteModal, setOpenDeleteModal] = React.useState(false);
     const openMenu = Boolean(anchorEl);
     const handleCloseMenu = (event: React.MouseEvent<HTMLElement>) => {
-        
+
         if (event.currentTarget.textContent === "Edit") {
             console.log("Edit");
-            setOpenModal(true);
+            setOpenEditModal(true);
         }
         if (event.currentTarget.textContent === "Delete") {
             console.log("Delete");
-            setOpenModal(true);
+            setOpenDeleteModal(true);
         }
-        setAnchorEl(null);
     }
     const handleClickMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -30,14 +30,14 @@ export const PopulatedSlotButton = (props: PopulatedSlotButtonProps) => {
     return (
         <Card sx={{ justifyContent: "space-around", display: "flex" }}>
             <Box>
-                <Typography variant="body2">{details.title}</Typography>
-                <Typography variant="caption">{details.description}</Typography>
+                <Typography variant="body2">{calendarEntry.title}</Typography>
+                <Typography variant="caption">{calendarEntry.description}</Typography>
             </Box>
             <Box>
                 <IconButton onClick={handleClickMenu}>
                     <MoreVert />
                 </IconButton>
-                
+
                 <Menu open={openMenu} anchorEl={anchorEl} onClose={handleCloseMenu}>
                     <MenuItem onClick={handleCloseMenu}>
                         <ListItemIcon>
@@ -45,12 +45,20 @@ export const PopulatedSlotButton = (props: PopulatedSlotButtonProps) => {
                         </ListItemIcon>
                         <ListItemText primary="Edit"></ListItemText>
                     </MenuItem>
+                    <EventModal calendarEntry = {calendarEntry} open={openEditModal} handleClose={() => { setOpenEditModal(false); setAnchorEl(null); }} />
                     <MenuItem onClick={handleCloseMenu}>
                         <ListItemIcon>
                             <Remove fontSize="small"></Remove>
                         </ListItemIcon>
                         <ListItemText primary="Delete"></ListItemText>
                     </MenuItem>
+                    <Dialog open={openDeleteModal} onClose={() => { setOpenDeleteModal(false); setAnchorEl(null); }}>
+                        <Typography sx={{padding: "1rem"}}>Are you sure you want to delete this event?</Typography>
+                        <Box sx={{ display: "flex", justifyContent: "space-around", padding: "1rem" }}>
+                            <IconButton onClick={() => { setOpenDeleteModal(false); setAnchorEl(null); }}>No</IconButton>
+                            <IconButton onClick={() => { setOpenDeleteModal(false); setAnchorEl(null); }}>Yes</IconButton>
+                        </Box>
+                    </Dialog>
                 </Menu>
             </Box>
         </Card>
