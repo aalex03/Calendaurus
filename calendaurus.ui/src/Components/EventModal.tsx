46a9ca7e64fcf50ab0b5311016453614b7fc30c1
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-03 11:18:05 +0300

    populate modal with existing data when editing

--- calendaurus.ui/src/Components/EventModal.tsx
+++ calendaurus.ui/src/Components/EventModal.tsx
@@ -1,12 +1,20 @@
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
 import { StaticDateTimePicker } from "@mui/x-date-pickers";
 import { on } from "events";
+import { ICalendarEntry } from "../types";
+import { useState } from "react";
 export type EventModalProps = {
     open: boolean;
-    handleClose: () => void;
+    handleClose: () => void,
+    calendarEntry? : ICalendarEntry
 }
 
 export const EventModal = (props : EventModalProps) => {
+    const [title, setTitle] = useState(props.calendarEntry?.title || "");
+    const [description, setDescription] = useState(props.calendarEntry?.description || "");
+    const [type, setType] = useState(props.calendarEntry?.type || 1);
+    const [location, setLocation] = useState(props.calendarEntry?.location || "");
+    const [start, setStart] = useState(props.calendarEntry?.start || new Date());
     return (
         <Dialog open={props.open} onClose={props.handleClose}>
             <DialogTitle>
@@ -14,10 +22,10 @@ export const EventModal = (props : EventModalProps) => {
             </DialogTitle>
             <DialogContent>
                 <Box sx={{display: "flex", flexDirection: "column", gap: "2"}}>
-                    <TextField label="Title" variant="standard"/>
+                    <TextField label="Title" variant="standard" value={title} onChange={e => setTitle(e.target.value)}/>
                     <FormControl>
                         <InputLabel>Type</InputLabel>
-                        <Select label="Type">
+                        <Select label="Type" value={type} onChange={e => setType(e.target.value)}>
                             <MenuItem value={1}>Course</MenuItem>
                             <MenuItem value={2}>Laboratory</MenuItem>
                             <MenuItem value={3}>Seminar</MenuItem>
@@ -25,8 +33,8 @@ export const EventModal = (props : EventModalProps) => {
                             <MenuItem value={5}>Exam</MenuItem>
                         </Select>
                     </FormControl>
-                    <TextField label="Description" variant="standard"/>
-                    <TextField label="Location" variant="standard"/>
+                    <TextField label="Description" variant="standard" value={description} onChange={e => setDescription(e.target.value)}/>
+                    <TextField label="Location" variant="standard" value={location} onChange={e => setLocation(e.target.value)}/>
                     <StaticDateTimePicker views={["day","hours"]} slotProps={{toolbar: {hidden: true}, actionBar: () => ({actions: []})}}></StaticDateTimePicker>
                 </Box>
             </DialogContent>
