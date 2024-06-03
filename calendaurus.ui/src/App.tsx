0a70074e0a58da01c3b56b6cb912b6385726663b
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-03 10:43:05 +0300

    remove unused imports

--- calendaurus.ui/src/App.tsx
+++ calendaurus.ui/src/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { LoginPage } from './Pages/LoginPage';
-import { Header } from './Components/Header';
 import { MainPage } from './Pages/MainPage';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import { Login } from '@mui/icons-material';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path="/" element={<LoginPage name={''} lastName={''} />} />
+          <Route path="/" element={<LoginPage/>} />
           <Route path="/home" element={<MainPage />} />
           <Route path="/*" element={<MainPage />} />
         </Routes>
