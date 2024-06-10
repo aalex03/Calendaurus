04fb9b0f58dfca21f0baeee5c1902406056c94b8
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-11 00:01:10 +0300

    add put and delete methods

---
+++ calendaurus.ui/src/Api/putCalendarEntry.ts
@@ -0,0 +1,16 @@
+import { IPublicClientApplication } from "@azure/msal-browser"
+import { prepareToken } from "./authUtils";
+import { ICalendarEntry } from "../types";
+
+export const postCalendarEntryMutation = async (instance : IPublicClientApplication, entry: ICalendarEntry) => {
+    const url = `http://localhost:5234/api/Calendar/${entry.id}`;
+    const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${await prepareToken(instance)}`
+        },
+        body: JSON.stringify(entry)
+    });
+    return await response.json();
+}
