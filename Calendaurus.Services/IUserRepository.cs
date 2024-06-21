3f5ce1f378e90b586c10261ca99a25dc4c66777f
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-06-21 17:41:51 +0300

    add user signup functionality in the service layer

--- Calendaurus.Services/IUserRepository.cs
+++ Calendaurus.Services/IUserRepository.cs
@@ -5,4 +5,5 @@ namespace Calendaurus.Services;
 public interface IUserRepository
 {
     Task<User?> GetByEmailAsync(string email);
+    Task<User> CreateAsync(string email);
 }
--- Calendaurus.Services/UserRepository.cs
+++ Calendaurus.Services/UserRepository.cs
@@ -12,6 +12,16 @@ public class UserRepository : IUserRepository
         _dbContext = dbContext;
     }
 
+    public async Task<User> CreateAsync(string email)
+    {
+        var user = await _dbContext.Users.AddAsync(new User{
+            Id = Guid.NewGuid(),
+            Email = email
+        });
+        await _dbContext.SaveChangesAsync();
+        return user.Entity;
+    }
+
     public async Task<User?> GetByEmailAsync(string email)
     {
         return await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
